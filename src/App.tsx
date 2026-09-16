cba29403189a279af5c2e1b3e9199ad5b6af22ee
annaabramovaa <abramovaana193@gmail.com>
2026-09-16 13:06:13 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,52 @@
-import React from 'react';
 import './App.scss';
+import './styles/fonts.scss';
+import { Header } from './components/Header';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { HomePage } from './modules/HomePage';
+import { Footer } from './components/Footer';
+import { CartPage } from './modules/CartPage';
+import { CartProvider } from './contexts/CartContext';
+import { FavoritesProvider } from './contexts/FavoritesContext';
+import { FavoritesPage } from './modules/FavoritesPage';
+import { PhonesPage } from './modules/PhonesPage/PhonesPage';
+import { NotFoundPage } from './modules/NotFoundPage';
+import { TabletsPage } from './modules/TabletsPage';
+import { AccessoriesPage } from './modules/AccessoriesPage';
+import { ProductDetailsPage } from './modules/ProductDetailsPage';
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
-  return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
-  );
-};
+export const App = () => (
+  <CartProvider>
+    <FavoritesProvider>
+      <HashRouter>
+        <div className="App">
+          <Header />
+          <main className="app_content">
+            <div className="container">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/home" element={<Navigate to="/" replace />} />
+                <Route path="/cart" element={<CartPage />} />
+                <Route path="/favorites" element={<FavoritesPage />} />
+                <Route path="/phones">
+                  <Route index element={<PhonesPage />} />
+                  <Route path=":productId" element={<ProductDetailsPage />} />
+                </Route>
+                <Route path="/tablets">
+                  <Route index element={<TabletsPage />} />
+                  <Route path=":productId" element={<ProductDetailsPage />} />
+                </Route>
+                <Route path="/accessories">
+                  <Route index element={<AccessoriesPage />} />
+                  <Route path=":productId" element={<ProductDetailsPage />} />
+                </Route>
+                <Route path="*" element={<NotFoundPage />} />
+              </Routes>
+            </div>
+          </main>
+          <Footer className="footer" />
+        </div>
+      </HashRouter>
+    </FavoritesProvider>
+  </CartProvider>
+);
